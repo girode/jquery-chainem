0aa22dedb98f308634f6d4ccc3431238706d12da
grode <grode@03051w7cdr.educacion.local>
2014-06-03 15:40:06 -0300

    Interface Proposals 1

---
+++ jquery.chainem.js
@@ -0,0 +1,15 @@
+/* jquery.chainem source v1.0 - 03/06/2014
+ * author: grode
+ * 
+ **/
+
+(function($)
+ {
+        $.chainem = function(options) 
+        {
+           console.log(options);
+        };
+        
+        // code
+
+})(jQuery);
